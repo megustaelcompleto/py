7a1c75682eb683a1970e065c1d906298e24e1465
megustaelcompleto <valosvivanco@gmail.com>
2025-10-22 09:25:35 -0300

    Add files via upload

--- Python/menu/practica.py
+++ Python/menu/practica.py
@@ -10,7 +10,7 @@ while True:
     print("[4] Mostrar registro")
     print("[5] Salida")
     
-    opcion = input("Ingrese su opoción [1-5]: ")
+    opcion = input("Ingrese su opción [1-5]: ")
     
     match opcion:
         case "1":
@@ -20,33 +20,31 @@ while True:
             nombre    = input("Ingrese su Nombre : ")
             curso     = input("Ingrese su Curso : ")
             
-            c = 0
-            print("Agrege 4 notas : ")
-            for i in range(4):
-                notas = float(input())
-                c = notas + c
-                if notas < 2 or notas > 7:
-                    print("Solo notas entre 2 y 7")
-                    c = 0
-                    break
-            promedio = c/4
-            if promedio >= 4:
-                estado = "APROBADO"
-            else:
-                estado = "DESAPROBADO"
-            if c == 0:
-                break
-            if curso == "" or nombre == "" or run == "":
-                print("")
-                print("NO debe ingresar datos en blanco...")
-                break
-            else:
-                persona = {"RUN": run, "NOMBRE": nombre, "CURSO": curso, "NOTAS": f"{promedio:.1f}", "ESTADO": estado}
-                registros.append(persona)
-                print("")
-                print("Se agrego registro en forma existosa... :)")
+            n1   = float(input("Nota 1 : "))
+            n2   = float(input("Nota 2 : "))
+            n3   = float(input("Nota 3 : "))
+            n4   = float(input("Nota 4 : "))
             
+            if (2 <= n1 <= 7) and (2 <= n2 <= 7) and (2 <= n3 <= 7) and (2 <= n4 <= 7):
+                promedio = round((n1+n2+n3+n4)/4, 2)
+                if promedio >= 4:
+                    estado = "APROBADO"
+                else:
+                    estado = "DESAPROBADO"
+                if curso == "" or nombre == "" or run == "":
+                    print("")
+                    print("NO debe ingresar datos en blanco...")
+                    break
+                else:
+                    persona = {"RUN": run, "NOMBRE": nombre, "CURSO": curso, "Nota 1": n1, "Nota 2": n2, "Nota 3": n3, "Nota 4": n4 ,"Promedio": f"{promedio:.1f}", "ESTADO": estado}
+                    registros.append(persona)
+                    print("")
+                    print("Se agrego registro en forma existosa...")
+            else:
+                print("Ingresar notas del 2 al 7")
+                break
         case "2":
+            print("")
             #Eliminar registro de una lista
             buscar_run = input("Ingrese su RUN para ser eliminado : ")
             encontra = False
@@ -61,40 +59,56 @@ while True:
                 
         case "3":
             #Editar o modificar datos
+            print("")
             buscar_run = input("Ingrese su RUN para ser modificado : ")
-            encontra = False
-            c = 0
+            encontra1 = False
+            
             for persona in registros:
                 if persona["RUN"]  ==  buscar_run:
+                    encontra1 = True
                     nuevo_nombre = input("Ingrese nuevo nombre : ")
                     nuevo_curso  = input("Ingrese nuevo curso : ")
-                    print("Agrege 4 notas : ")
-                    for i in range(4):
-                        nueva_nota = float(input())
-                        c = nueva_nota + c
-                        if nueva_nota < 2 or nueva_nota > 7:
-                            print("Solo notas entre 2 y 7")
-                            break
-                    promedio = c/4
-                    if promedio >= 4:
-                        nuevo_estado = "APROBADO"
-                    elif promedio < 4:
-                        nuevo_estado = "DESAPROBADO"
-                    if nuevo_nombre:
-                        persona["NOMBRE"] = nuevo_nombre
+                    print("")
+                    print("Ingrese un 0 para mantener la nota original")
+                    nu1 = float(input(f"Nueva nota 1 (actual: {persona["Nota 1"]}) "))
+                    nu2 = float(input(f"Nueva nota 2 (actual: {persona["Nota 2"]}) "))
+                    nu3 = float(input(f"Nueva nota 3 (actual: {persona["Nota 3"]}) "))
+                    nu4 = float(input(f"Nueva nota 4 (actual: {persona["Nota 4"]}) "))
+                    if nu1 == 0:
+                        nu1 = persona["Nota 1"]
+                    if nu2 == 0:
+                        nu2 = persona["Nota 2"]
+                    if nu3 == 0:
+                        nu3 = persona["Nota 3"]
+                    if nu4 == 0:
+                        nu4 = persona["Nota 4"]
+                    
+                    if (2 <= nu1 <= 7) and (2 <= nu2 <= 7) and (2 <= nu3 <= 7) and (2 <= nu4 <= 7):
+                        nuevo_promedio = round((nu1+nu2+nu3+nu4)/4, 2)
+                        if nuevo_promedio >= 4:
+                            nuevo_estado = "APROBADO"
+                        elif nuevo_promedio < 4:
+                            nuevo_estado = "DESAPROBADO"
+                        if nuevo_nombre:
+                            persona["NOMBRE"]   = nuevo_nombre
                         if nuevo_curso:
-                            persona["CURSO"] = nuevo_curso
-                            if nueva_nota:
-                                persona["NOTAS"] = nueva_nota
-                                if nuevo_estado:
-                                    
-                                    persona["ESTADO"] = nuevo_estado
+                            persona["CURSO"]    = nuevo_curso
+                        if nuevo_estado:
+                            persona["ESTADO"]   = nuevo_estado
+                        if nuevo_promedio:
+                            persona["Promedio"] = nuevo_promedio
+                    else:
+                        print("Ingrese notas del 2 al 7")
+                        break
+                    print("")
+                    print("Cambios realizados con Exito...!!!")        
+                    break
+            if not encontra1:
+                print("")
+                print("RUN no encrontrado")
+                print("")
+                    
                 
-                print("Cambios realizados con Exito...!!!")        
-                encontra = True
-                break
-            if not encontra:
-                print("No se encuetra registro o empleado para ser modifico..!!")            
                 
                 
         case "4":
@@ -111,5 +125,5 @@ while True:
             print("Opcion NO valida")
             print("")
 
-print("Programa terminado.... muchas gracias....")
+print("Programa terminado...")
 
